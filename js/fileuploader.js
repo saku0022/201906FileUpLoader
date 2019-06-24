6f3cd93666b3a141d14abcf616fa7430e4dd9ef7
saku0022 <52141729+saku0022@users.noreply.github.com>
2019-06-24 09:03:16 +0900

    20190624

---
+++ js/fileuploader.js
@@ -0,0 +1,65 @@
+//　fileuploader.tplより
+// 「全て選択」チェックで全てにチェック付く
+function AllChecked(flg){
+	if (document.file_list.test instanceof RadioNodeList) {	// 配列になっているか、判定
+		// チェックボックスが複数の場合
+		for (var i=0; i < document.file_list.test.length; i++){
+			document.file_list.test[i].checked = flg;
+		}
+	} else {
+		// チェックボックスが１つだけの場合
+		document.file_list.test.checked = flg;
+	}
+}
+
+function download(fname) {
+	alert(fname +' をダウンロードします。 ');
+	document.download_form.fname.value = fname;
+	document.download_form.submit();
+}
+
+function delfile() {
+	// var files = 'DB_Connect.php/index.php/MySmarty.class.php';
+	var files = '';
+
+	if (document.file_list.test instanceof RadioNodeList) {	// 配列になっているか、判定
+		// チェックボックスが複数の場合
+		for (var i=0; i < document.file_list.test.length; i++) {
+			if (document.file_list.test[i].checked) {
+				if (files.length > 0) {
+					files+= '/';
+				}
+				files+= document.file_list.test[i].value;
+			}
+		}
+	} else {
+		// チェックボックスが１つだけの場合
+		files+= document.file_list.test.value;
+	}
+
+	if (files.length > 0) {
+		if(window.confirm('ファイルを削除します。')){
+			document.delete_form.fname.value = files;
+			document.delete_form.submit();      // value の　files をsubmit している
+		}
+	} else {
+		alert('ファイルを選択してください。');
+	}
+}
+
+//binds to onchange event of your input field
+$('#myFile').bind('change', function() {
+
+  //this.files[0].size gets the size of your file.
+  alert(this.files[0].size);
+
+});
+
+function check(){
+	var fileList = document.getElementById("sFiles").files;
+	var list = "";
+	for(var i=0; i<fileList.length; i++){
+	list += "[" + fileList[i].size + " bytes]" + fileList[i].name + "<br>";
+	}
+	document.getElementById("result").innerHTML = list;
+	}
